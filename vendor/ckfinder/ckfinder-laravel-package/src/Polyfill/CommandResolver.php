a1e44375a6b38e763bd48dedc5661594b4563902
wd-kajalsaini <128365707+wd-kajalsaini@users.noreply.github.com>
2024-03-20 16:15:20 +0530

    initial-commit

--- vendor/ckfinder/ckfinder-laravel-package/src/Polyfill/CommandResolver.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace CKSource\CKFinderBridge\Polyfill;
-
-use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
-
-class CommandResolver extends \CKSource\CKFinder\CommandResolver implements ArgumentResolverInterface {}
