9a7040dc99e92cda975b6dead86229d74b488fa7
wd-kajalsaini <128365707+wd-kajalsaini@users.noreply.github.com>
2024-04-05 12:32:30 +0530

    fixes

--- resources/views/static_pages/privacyPolicy.blade.php
+++ resources/views/static_pages/privacyPolicy.blade.php
@@ -1,15 +1,108 @@
-<center><h2>Privacy Policy</h2></center>
 
-<p>Your privacy is important to us. It is our policy to respect your privacy regarding any information we may collect from you across our website, example.com, and other sites we own and operate.</p>
-
-<p>We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
-
-<p>We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorized access, disclosure, copying, use, or modification.</p>
-
-<p>We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
-
-<p>Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
-
-<p>You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
-
-<p>Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
+@extends('layouts.static_pages')
+@section('content')
+<section>
+    <div class="row align-items-center">
+      <div class="col-12">
+        <div class="text-center mb-5">
+          <h1>Privacy Policy</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-8 mx-auto pb-5">
+        <div class="ss__content-box">
+          <h3 class="ss__h3heading">Methods of processing</h3>
+          <p class="ss__para_sm">The Owner takes appropriate security measures to prevent unauthorized access,
+            disclosure,
+            modification, or unauthorized destruction of the Data. The Data processing is carried out using computers
+            and/or
+            IT enabled tools, following organizational procedures and modes strictly related to the purposes indicated.
+            In
+            addition to the Owner, in some cases, the Data may be accessible to certain types of persons in charge,
+            involved
+            with the operation of this Website (administration, sales, marketing, legal, system administration) or
+            external
+            parties (such as third-party technical service providers, mail carriers, hosting providers, IT companies,
+            communications agencies) appointed, if necessary, as Data Processors by the Owner. The updated list of these
+            parties may be requested from the Owner at any time.
+          </p>
+          <h3 class="ss__h3heading">Legal basis of processing</h3>
+          <p class="ss__para_sm">The Owner may process Personal Data relating to Users if one of the following applies:
+          </p>
+          <ul class="ss_points mb-5">
+            <li>Users have given their consent for one or more specific purposes. Note: Under some legislations the
+              Owner
+              may be allowed to process Personal Data until the User objects to such processing (“opt-out”), without
+              having
+              to rely on consent or any other of the following legal bases. This, however, does not apply, whenever the
+              processing of Personal Data is subject to data protection law;
+            </li>
+            <li>provision of Data is necessary for the performance of an agreement with the User and/or for any
+              pre-contractual obligations thereof;
+            </li>
+            <li>processing is necessary for compliance with a legal obligation to which the Owner is subject;</li>
+            <li>processing is related to a task that is carried out in the public interest or in the exercise of
+              official
+              authority vested in the Owner;
+            </li>
+            <li>processing is necessary for the purposes of the legitimate interests pursued by the Owner or by a third
+              party.
+            </li>
+          </ul>
+          <p class="ss__para_sm">In any case, the Owner will gladly help to clarify the specific legal basis that
+            applies to
+            the processing, and in particular whether the provision of Personal Data is a statutory or contractual
+            requirement, or a requirement necessary to enter into a contract.
+          </p>
+          <h3 class="ss__h3heading">Place</h3>
+          <p class="ss__para_sm">The Data is processed at the Owner’s operating offices and in any other places where
+            the
+            parties involved in the processing are located
+          </p>
+          <p class="ss__para_sm">Depending on the User’s location, data transfers may involve transferring the User’s
+            Data
+            to a country other than their own. To find out more about the place of processing of such transferred Data,
+            Users can check the section containing details about the processing of Personal Data. Users are also
+            entitled to
+            learn about the legal basis of Data transfers to a country outside the jurisdition or to any international
+            organization governed by public international law or set up by two or more countries, such as the UN, and
+            about
+            the security measures taken by the Owner to safeguard their Data.
+            If any such transfer takes place, Users can find out more by checking the relevant sections of this document
+            or
+            inquire with the Owner using the information provided in the contact section.
+          </p>
+          <p class="ss__para_sm">If any such transfer takes place, Users can find out more by checking the relevant
+            sections
+            of this document or inquire with the Owner using the information provided in the contact section.
+          </p>
+          <h3 class="ss__h3heading">Retention time</h3>
+          <p class="ss__para_sm">Personal Data shall be processed and stored for as long as required by the purpose they
+            have been collected for.
+          </p>
+          <p class="ss__para_sm">Therefore:</p>
+          <ul class="ss_points mb-5">
+            <li>Personal Data collected for purposes related to the performance of a contract between the Owner and the
+              User
+              shall be retained until such contract has been fully performed.
+            </li>
+            <li>The Owner may be allowed to retain Personal Data for a longer period whenever the User has given consent
+              to
+              such processing, as long as such consent is not withdrawn. Furthermore, the Owner may be obliged to retain
+              Personal Data for a longer period whenever required to do so for the performance of a legal obligation or
+              upon
+              order of an authority.
+            </li>
+          </ul>
+          <p class="ss__para_sm">Once the retention period expires, Personal Data shall be deleted. Therefore, the right
+            to
+            access, the right to erasure, the right to rectification and the right to data portability cannot be
+            enforced
+            after expiration of the retention period.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  @endsection
